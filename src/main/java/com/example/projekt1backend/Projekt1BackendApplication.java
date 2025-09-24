91d33af77e06423f1701bebefede94f89d6a1f31
Zahaawii <zahaawii@gmail.com>
2025-09-24 09:19:22 +0200

    init project

---
+++ src/main/java/com/example/projekt1backend/Projekt1BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.projekt1backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekt1BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projekt1BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projekt1backend/Projekt1BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projekt1backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projekt1BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
