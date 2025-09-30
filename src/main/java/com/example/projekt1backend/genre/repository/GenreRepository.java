7438ccd1e69870093ab4ed378aa4cb600c52cf18
Simon <sipe0005@stud.kea.dk>
2025-09-30 10:56:43 +0200

    CRUD functionality for movies

--- src/main/java/com/example/projekt1backend/genre/repository/GenreRepository.java
+++ src/main/java/com/example/projekt1backend/genre/repository/GenreRepository.java
@@ -1,5 +1,6 @@
-package com.example.projekt1backend.genre;
+package com.example.projekt1backend.genre.repository;
 
+import com.example.projekt1backend.genre.entity.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GenreRepository extends JpaRepository<Genre, Integer> {
--- src/main/java/com/example/projekt1backend/movie/repository/MovieRepository.java
+++ src/main/java/com/example/projekt1backend/movie/repository/MovieRepository.java
@@ -1,5 +1,6 @@
-package com.example.projekt1backend.movie;
+package com.example.projekt1backend.movie.repository;
 
+import com.example.projekt1backend.movie.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
