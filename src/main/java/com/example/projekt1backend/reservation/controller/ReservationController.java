eca869f4bd1597bd67318bf3d3ec69c82cdb5908
Zahaawii <zahaawii@gmail.com>
2025-10-01 11:01:42 +0200

    adding DTO

--- src/main/java/com/example/projekt1backend/reservation/controller/ReservationController.java
+++ src/main/java/com/example/projekt1backend/reservation/controller/ReservationController.java
@@ -3,8 +3,11 @@ package com.example.projekt1backend.reservation.controller;
 
 import com.example.projekt1backend.customer.Customer;
 import com.example.projekt1backend.customer.CustomerService;
+import com.example.projekt1backend.reservation.dto.ReservationDTO;
 import com.example.projekt1backend.reservation.entity.Reservation;
 import com.example.projekt1backend.reservation.service.ReservationService;
+import com.example.projekt1backend.screening.model.Screening;
+import com.example.projekt1backend.screening.service.ScreeningService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,10 +20,12 @@ public class ReservationController {
 
     private final ReservationService reservationService;
     private final CustomerService customerService;
+    private final ScreeningService screeningService;
 
-    public ReservationController(ReservationService reservationService, CustomerService customerService) {
+    public ReservationController(ReservationService reservationService, CustomerService customerService, ScreeningService screeningService) {
         this.reservationService = reservationService;
         this.customerService = customerService;
+        this.screeningService = screeningService;
     }
 
     @GetMapping("/reservations")
@@ -29,14 +34,15 @@ public class ReservationController {
     }
 
     @PostMapping("/reservations")
-    public ResponseEntity<?> createReservation(@RequestBody Reservation reservation) {
-        //Tester lige nu med en bruger jeg selv opretter for at se om det virker som det skal i backend.
-        //Hvilke det gør, skal bare lige finde ud af hvordan jeg får id fra brugeren med.
-        //Plus det skal være i service klassen det her, ikke i controller. To be continued....
-        Customer customer = customerService.findById(1);
-        //Screening screening = something here
+    public ResponseEntity<String> createReservation(@RequestBody ReservationDTO reservationDTO) {
+        
+        Customer customer = customerService.findById(reservationDTO.customerID());
+        Screening screening = screeningService.findById(reservationDTO.screeningID());
+        Reservation reservation = new Reservation();
+
         reservation.setCustomer(customer);
+        reservation.setScreening(screening);
         reservationService.addReservation(reservation);
-        return new ResponseEntity<>(reservation + ": has been created", HttpStatus.CREATED);
+        return new ResponseEntity<>("Reservation has been created" , HttpStatus.CREATED);
     }
 }
