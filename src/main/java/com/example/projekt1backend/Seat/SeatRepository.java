dd6c1273f7b8da09b85939fbc6568a9cb6b72ea1
Zahaawii <141314605+Zahaawii@users.noreply.github.com>
2025-10-01 11:33:36 +0200

    Delete src/main/java/com/example/projekt1backend/Seat directory

--- src/main/java/com/example/projekt1backend/Seat/SeatRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.projekt1backend.Seat;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SeatRepository extends JpaRepository<Seat, Integer> {
-}
