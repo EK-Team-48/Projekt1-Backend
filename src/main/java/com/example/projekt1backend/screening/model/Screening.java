71b9853e8118fc5d67160f48dace2dff302757e2
Zahaawii <zahaawii@gmail.com>
2025-10-06 10:07:17 +0200

    changing rest API to give the data i need in the frontend

--- src/main/java/com/example/projekt1backend/screening/model/Screening.java
+++ src/main/java/com/example/projekt1backend/screening/model/Screening.java
@@ -101,4 +101,11 @@ public class Screening {
         this.price = price;
     }
 
+    public Set<Seat> getSeats() {
+        return seats;
+    }
+
+    public void setSeats(Set<Seat> seats) {
+        this.seats = seats;
+    }
 }
