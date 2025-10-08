0c8aa9fccf44a3854e8e1c5bb3307c8d58c68cd5
Simon <sipe0005@stud.kea.dk>
2025-10-08 11:08:19 +0200

    crud funktionalitet tilføjet til Genre

--- src/main/java/com/example/projekt1backend/genre/service/GenreService.java
+++ src/main/java/com/example/projekt1backend/genre/service/GenreService.java
@@ -6,6 +6,7 @@ import com.example.projekt1backend.genre.repository.GenreRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GenreService {
@@ -19,5 +20,17 @@ public class GenreService {
 
     public List<Genre> getAll() {
         return genreRepository.findAll();
+    }
+
+    public Optional<Genre> findById(Integer id) {
+        return genreRepository.findById(id);
+    }
+
+    public Genre save(Genre genre) {
+        return genreRepository.save(genre);
+    }
+
+    public void deleteById(Integer id) {
+        genreRepository.deleteById(id);
     }
 }
